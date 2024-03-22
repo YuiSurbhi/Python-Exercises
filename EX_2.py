7a3125b6d3fe511ca6d5b00020acfc163f2ac8e3
YuiSurbhi <163987279+YuiSurbhi@users.noreply.github.com>
2024-03-22 09:47:52 +0000

    ...

--- EX_2.py
+++ EX_2.py
@@ -1,1 +1,11 @@
-cdsfvc
+print("Band name generator by Surbhi Verma.");
+
+city_name = input("Which city do you live in : ");
+
+pet_name = input("What is the name of your pet : ");
+
+band_name = city_name + " " + pet_name;
+
+print("Our band name is : ",band_name);
+
+
