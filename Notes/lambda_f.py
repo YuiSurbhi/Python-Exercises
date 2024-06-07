da119d8f2746be89f3cfc0e5ebe619ff008b1c3a
YuiSurbhi <163987279+YuiSurbhi@users.noreply.github.com>
2024-06-07 10:23:31 +0000

    committed

---
+++ Notes/lambda_f.py
@@ -0,0 +1,26 @@
+#lambda function 
+
+def appl(fx,value):
+    return fx(value)
+
+answer = lambda a:a**2
+cube = lambda x:x*x*x
+avg = lambda x,y,z: (x+y+z)/3
+
+print(answer(2))
+print(cube(3))
+print(avg(2,4,7))
+
+print(appl(cube,6))
+
+my_function = lambda x: x*5
+print(my_function(3))
+print(my_function(4))
+print(my_function(8))
+ 
+'''def my_function(a):
+    return a**2
+print(my_function(6))'''
+
+my_function = lambda a:a**2
+print(my_function(2))
