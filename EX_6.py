e4521051ca580f8459c7ba447f5522362c14f5f9
YuiSurbhi <163987279+YuiSurbhi@users.noreply.github.com>
2024-06-07 10:52:27 +0000

    committed

---
+++ EX_6.py
@@ -0,0 +1,48 @@
+#map(),filter(), and reduce()
+
+#MAP()
+def cube(a):
+    return a*a*a
+
+l = [1,2,3,4,5,6]
+newl = []
+for item in l:
+    newl.append(cube(item))
+
+print(newl)
+
+#using lambda function 
+newl = list(map(lambda x:x*x*x,l))
+print(newl)
+
+#FILTER()
+l = [1,2,3,4,5,6]
+
+def filter_function(l):
+    return l>4
+
+newl = list(filter(filter_function,l))
+print(newl)
+
+#REDUCE
+from functools import reduce
+
+#list of numbers
+l = [1,2,3,4,5]
+
+#calculate sum of numbers using reduce function
+sum = reduce(lambda x,y:x+y,l)
+
+#print the sum
+print(sum)
+
+#using def keyword 
+from functools import reduce
+
+def mysum(x,y):
+    return x+y
+
+l = [1,2,3,4,5]
+sum = reduce(mysum,l)
+
+print(mysum)
--- Notes/lambda_f.py
+++ Notes/lambda_f.py
@@ -1,8 +1,6 @@
 #lambda_function 
 
-def appl(fx,value):
-    return fx(value)
-
+#1)
 answer = lambda a:a**2
 cube = lambda x:x*x*x
 avg = lambda x,y,z: (x+y+z)/3
@@ -11,16 +9,24 @@ print(answer(2))
 print(cube(3))
 print(avg(2,4,7))
 
+#passing function in a function 
+def appl(fx,value):
+    return fx(value)
+
 print(appl(cube,6))
 
+#2)
 my_function = lambda x: x*5
 print(my_function(3))
 print(my_function(4))
 print(my_function(8))
  
-'''def my_function(a):
+#using def keyword 
+def my_function(a):
     return a**2
-print(my_function(6))'''
 
+print(my_function(6))
+
+#using lambda 
 my_function = lambda a:a**2
 print(my_function(2))
